48fd19bbfc021528a6fc35cecdf51eb2ae626b34
Lotan <lotan.mash@gmail.com>
2021-03-18 23:02:52 +0200

    lotan in da house!

--- src/cmps/tour-filter.vue
+++ src/cmps/tour-filter.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="tour-filter">
-    <form @submit.prevent="getAns()" action="" >
+    <form @submit.prevent="getAns()" action="">
       <div class="con">
-      <el-input
-        class="destination-input"
-        placeholder="Destination"
-        v-model="desVal"
-        clearable
-      >
-      </el-input>
+        <el-input
+          class="destination-input"
+          placeholder="Destination"
+          v-model="desVal"
+          clearable
+        >
+        </el-input>
       </div>
       <el-date-picker
         v-model="timeVal"
@@ -16,8 +16,13 @@
         align="right"
         start-placeholder="Start Date"
         end-placeholder="End Date"
-        :default-time="['12:00:00', '08:00:00']">
+        :default-time="['12:00:00', '08:00:00']"
+      >
       </el-date-picker>
+      <div @click="openRange()">Price</div>
+      <div v-if="open" class="range-box">
+      <el-slider v-model="value" range :max="100"> </el-slider>
+      </div>
       <el-button class="save-btn" icon="el-icon-search" circle></el-button>
     </form>
   </section>
@@ -27,11 +32,21 @@
 export default {
   data() {
     return {
-      timeVal: '',
-      desVal: ''
+      timeVal: "",
+      desVal: "",
+      value: 0,
+      open: false
     };
   },
   methods: {
+    openRange() {
+      if(this.open === true) {
+        this.open = false
+        console.log(this.open);
+      } else {
+        this.open = true
+      }
+    }
     // getAns() {
     //   var destination = this.desVal;
     //   var startTimestamp = this.timeVal[0].getTime();
