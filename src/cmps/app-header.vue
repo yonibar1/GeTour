3f532334119cf841ed105e8aad1c87717fa2300f
Lotan <lotan.mash@gmail.com>
2021-03-19 13:43:37 +0200

    lotan in da house!

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,14 +1,9 @@
 <template >
-  <section class="app-header" ref="header">
+  <section class="app-header" ref="header" >
     <div class="logo-container">
       <router-link to="/">
-        <img
-          v-if="!isScrolling"
-          ref="logoImg"
-          src="@/assets/logo-w.svg"
-          alt=""
-        />
-        <img v-else src="@/assets/logo1.svg" alt="" />
+        <img v-if="!isScrolling" src="@/assets/logo-w.svg" alt="" />
+        <img v-else src="@/assets/logo1.svg" alt=""/>
       </router-link>
     </div>
     <nav>
@@ -27,34 +22,38 @@ export default {
       isScrolling: false,
     };
   },
-  methos: {
+  methods: {
     onScroll() {
-      if (window.scrollY > 10) {
-        console.log("yesssss");
-        this.isScrolling = true;
-        this.$refs.header.style.backgroundColor = "white";
-      } else {
-        this.isScrolling = false;
-        this.$refs.header.style.backgroundColor = "transparent";
-      }
+      console.log($route);
+      //   console.log(this.$refs);
+      //   console.log("hoh");
+      //   if (window.scrollY > 10) {
+      //     console.log("yesssss");
+      //     this.isScrolling = true;
+      //     this.$refs.header.style.backgroundColor = "white";
+      //   } else {
+      //     this.isScrolling = false;
+      //     this.$refs.header.style.backgroundColor = "transparent";
+      //   }
     },
   },
-  watch: {
-    $route: {
-      deep: true,
-      immediate: true,
-      handler: function (val) {
-        if (val.fullPath === "/") {
-          console.log("im home");
-          window.addEventListener("scroll", this.onScroll);
-          this.$refs.header.classList.remove("relative-pos");
-        } else {
-          console.log("not home");
-          this.$refs.header.classList.add("relative-pos");
-          window.removeEventListener("scroll", this.onScroll);
-        }
-      },
-    },
-  },
+  // watch: {
+  //   $route: {
+  //     deep: true,
+  //     immediate: true,
+  //     handler: function (val) {
+  //       if (val.fullPath === "/") {
+  //         console.log("im home");
+  //         window.addEventListener("scroll", this.onScroll);
+  //         console.log(this.$refs);
+  //         // this.$refs.header.classList.remove("relative-pos");
+  //       } else {
+  //         console.log("not home");
+  //         this.$refs.header.classList.add("relative-pos");
+  //         window.removeEventListener("scroll", this.onScroll);
+  //       }
+  //     },
+  //   },
+  // },
 };
 </script>
