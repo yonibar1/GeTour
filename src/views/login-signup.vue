048b02468f0ebbeb458c225e85133344fa03e8df
yonibar1 <yonibar1999@gmail.com>
2021-03-21 14:53:02 +0200

    Yoni

--- src/views/login-signup.vue
+++ src/views/login-signup.vue
@@ -1,33 +1,62 @@
 <template>
   <section class="login-signup">
-    <!-- Sign Up -->
-    <h2>Sign Up</h2>
-    <form @submit.prevent="signup">
+    <img class="logo" src="../assets/logo-signs-green.svg" alt="" />
+    <form v-if="isLogin">
+      <img class="auth-logo" src="../assets/Login.svg" alt="" />
       <label>
-        <input type="file" />
+        <input
+          type="text"
+          required
+          v-model="loginData.username"
+          placeholder="User Name"
+        />
       </label>
       <label>
-        <input type="text" placeholder="Full Name" />
+        <input
+          type="password"
+          required
+          v-model="loginData.password"
+          placeholder="Password"
+        />
       </label>
-      <label>
-        <input type="text" placeholder="User Name" />
-      </label>
-      <label>
-        <input type="password" placeholder="Password" />
-      </label>
-      <button>Sign Up</button>
+      <el-button round plain type="success" @click="login">Login</el-button>
+      <a class="set-form-link" @click="setForm">Dont Have an account yet?</a>
     </form>
-
-    <!-- Login -->
-    <h2>login</h2>
-    <form @submit.prevent="login">
-      <label>
-        <input type="text" placeholder="User Name" />
+    <form v-else>
+      <img class="auth-logo" src="../assets/Signup.svg" alt="" />
+      <label class="profile-picture-upload">
+        <i class="fas fa-cloud-upload-alt">
+          <input
+            type="file"
+            required
+            @change="handleFile"
+            class="file-input"
+            multiple
+          />
+        </i>
       </label>
       <label>
-        <input type="password" placeholder="Password" />
+        <input required v-model="signupData.fullname" placeholder="Full Name" />
       </label>
-      <button>Login</button>
+      <label>
+        <input
+          type="text"
+          required
+          v-model="signupData.username"
+          placeholder="User Name"
+        />
+      </label>
+      <label>
+        <input
+          type="password"
+          required
+          v-model="signupData.password"
+          placeholder="Password"
+          show-password
+        />
+      </label>
+      <el-button plain round type="success">Sign Up</el-button>
+      <a class="set-form-link" @click="setForm">Already Have an account?</a>
     </form>
   </section>
 </template>
@@ -35,8 +64,28 @@
 <script>
 export default {
   name: "login-signup",
+  data() {
+    return {
+      loginData: {
+        username: "",
+        password: "",
+      },
+      signupData: {
+        imgUrl: "",
+        fullname: "",
+        username: "",
+        password: "",
+      },
+      isLogin: true,
+    };
+  },
+  computed: {},
   methods: {
+    setForm() {
+      this.isLogin = !this.isLogin;
+    },
     signup() {
+      console.log(this.signupData);
       // this.$store
       //   .dispatch({ type: "saveUser", user: this.loggedinUser })
       //   .then(() => {
@@ -44,11 +93,15 @@ export default {
       //   });
     },
     login() {
+      console.log(this.loginData);
       // this.$store
       //   .dispatch({ type: "saveUser", user: this.loggedinUser })
       //   .then(() => {
       //     // this.$router.push("/tour-app");
       //   });
+    },
+    handleFile(ev) {
+      console.log(ev.target.files);
     },
   },
 };
