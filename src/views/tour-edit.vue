fb8aa42569639a1d8110852a9c8ce381765776e1
avi lugassi <avilugassi23@gmail.com>
2021-03-27 10:57:20 +0300

    avi

--- src/views/tour-edit.vue
+++ src/views/tour-edit.vue
@@ -1,213 +1,214 @@
 <template>
-  <section class="tour-edit">
-    <div class="form-edit">
-      <h3>Plan Your Special Tour</h3>
-      <el-form v-if="tourToEdit" action="">
-        <el-form-item>
-          <label for="title">Title</label>
-          <el-input
-            id="title"
-            v-model="tourToEdit.title"
-            placeholder="Title"
-            type="text"
-          />
-        </el-form-item>
-        <el-form-item>
-          <label for="country">Country</label>
-          <el-input
-            id="country"
-            v-model="tourToEdit.country"
-            placeholder="Country"
-            type="text"
-          />
-        </el-form-item>
-        <el-form-item>
-          <label for="price">Price</label>
-          <el-input-number
-            id="price"
-            class="input-number"
-            v-model.number="tourToEdit.price"
-            placeholder="Price"
-            type="number"
-          />
-        </el-form-item>
-        <el-form-item>
-          <label for="capacity">Capacity</label>
-          <el-input-number
-            id="capacity"
-            v-model.number="tourToEdit.capacity"
-            placeholder="Capacity"
-            type="number"
-          />
-        </el-form-item>
+    <section class="tour-edit">
+        <div class="form-edit">
+            <h3>Plan Your Special Tour</h3>
+            <el-form v-if="tourToEdit" action="">
+                <el-form-item>
+                    <label for="title">Title</label>
+                    <el-input
+                        id="title"
+                        v-model="tourToEdit.title"
+                        placeholder="Title"
+                        type="text"
+                    />
+                </el-form-item>
+                <el-form-item>
+                    <label for="country">Country</label>
+                    <el-input
+                        id="country"
+                        v-model="tourToEdit.country"
+                        placeholder="Country"
+                        type="text"
+                    />
+                </el-form-item>
+                <el-form-item>
+                    <label for="price">Price</label>
+                    <el-input-number
+                        id="price"
+                        class="input-number"
+                        v-model.number="tourToEdit.price"
+                        placeholder="Price"
+                        type="number"
+                    />
+                </el-form-item>
+                <el-form-item>
+                    <label for="capacity">Capacity</label>
+                    <el-input-number
+                        id="capacity"
+                        v-model.number="tourToEdit.capacity"
+                        placeholder="Capacity"
+                        type="number"
+                    />
+                </el-form-item>
 
-        <el-form-item>
-          <el-date-picker
-            v-model="tourToEdit.startedAt"
-            type="date"
-            placeholder="Pick a day"
-            value-format="timestamp"
-          >
-          </el-date-picker>
-        </el-form-item>
+                <el-form-item>
+                    <el-date-picker
+                        v-model="tourToEdit.startedAt"
+                        type="date"
+                        placeholder="Pick a day"
+                        value-format="timestamp"
+                    >
+                    </el-date-picker>
+                </el-form-item>
 
-        <el-form-item>
-          <label for="daysCount">Days Count</label>
-          <el-input-number
-            id="daysCount"
-            v-model.number="tourToEdit.daysCount"
-            placeholder="Days"
-            type="number"
-          />
-        </el-form-item>
-        <el-form-item>
-          <label for="Difficulty">Difficulty</label>
-          <el-input-number
-            id="Difficulty"
-            v-model.number="tourToEdit.difficulty"
-            placeholder="Difficulty"
-            type="number"
-          />
-        </el-form-item>
-        <el-form-item>
-          <label for="tags">Tags</label>
-          <el-select
-            id="tags"
-            v-model="tourToEdit.tags"
-            multiple
-            placeholder="Select"
-          >
-            <el-option
-              v-for="option in options"
-              :key="option.id"
-              :label="option.label"
-              :value="option.value"
-            >
-            </el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <label for="description">Description</label>
-          <el-input
-            id="description"
-            type="textarea"
-            maxlength="100"
-            show-word-limit
-            v-model="tourToEdit.description"
-            :autosize="{ minRows: 2, maxRows: 6 }"
-            placeholder="Description"
-          >
-          </el-input>
-        </el-form-item>
-        <el-form-item>
-          <upload-image @save="saveImg" />
-          <div class="imgs-container">
-            {{ tourToEdit.imgs.length }} / 5
-            <img
-              class="uploaded-img"
-              v-for="(url, idx) in tourToEdit.imgs"
-              :key="idx"
-              :src="url"
-            />
-          </div>
-        </el-form-item>
-        <label for="tourMap"> Enter Tour Checkpoints </label>
-        <tour-map id="tourMap" @setCheckPoints="setCheckPoints" />
-        <el-button @click="saveTour" v-if="tourToEdit._id"
-          >Update Tour</el-button
-        >
-        <el-button @click="saveTour" v-else>Create Tour</el-button>
-      </el-form>
-    </div>
-  </section>
+                <el-form-item>
+                    <label for="daysCount">Days Count</label>
+                    <el-input-number
+                        id="daysCount"
+                        v-model.number="tourToEdit.daysCount"
+                        placeholder="Days"
+                        type="number"
+                    />
+                </el-form-item>
+                <el-form-item>
+                    <label for="Difficulty">Difficulty</label>
+                    <el-input-number
+                        id="Difficulty"
+                        v-model.number="tourToEdit.difficulty"
+                        placeholder="Difficulty"
+                        type="number"
+                    />
+                </el-form-item>
+                <el-form-item>
+                    <label for="tags">Tags</label>
+                    <el-select
+                        id="tags"
+                        v-model="tourToEdit.tags"
+                        multiple
+                        placeholder="Select"
+                    >
+                        <el-option
+                            v-for="option in options"
+                            :key="option.id"
+                            :label="option.label"
+                            :value="option.value"
+                        >
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item>
+                    <label for="description">Description</label>
+                    <el-input
+                        id="description"
+                        type="textarea"
+                        maxlength="600"
+                        show-word-limit
+                        v-model="tourToEdit.description"
+                        :autosize="{ minRows: 2, maxRows: 6 }"
+                        placeholder="Description"
+                    >
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <upload-image @save="saveImg" />
+                    <div class="imgs-container">
+                        {{ tourToEdit.imgs.length }} / 5
+                        <img
+                            class="uploaded-img"
+                            v-for="(url, idx) in tourToEdit.imgs"
+                            :key="idx"
+                            :src="url"
+                        />
+                    </div>
+                </el-form-item>
+                <label for="tourMap"> Enter Tour Checkpoints </label>
+                <tour-map id="tourMap" @setCheckPoints="setCheckPoints" />
+                <el-button @click="saveTour" v-if="tourToEdit._id"
+                    >Update Tour</el-button
+                >
+                <el-button @click="saveTour" v-else>Create Tour</el-button>
+            </el-form>
+        </div>
+    </section>
 </template>
 
 <script>
-import tourMap from "../cmps/tour-map";
-import uploadImage from "../cmps/upload-image";
+import tourMap from '../cmps/tour-map';
+import uploadImage from '../cmps/upload-image';
 export default {
-  name: "tourEdit",
-  data() {
-    return {
-      tourToEdit: null,
-      options: [
-        {
-          label: "Water Trail",
-          value: { txt: "Water Trail", class: "fas fa-water" },
-          id: "t101",
-        },
-        {
-          label: "Hiking Tour",
-          value: { txt: "Hiking Tour", class: "fas fa-hiking" },
-          id: "t102",
-        },
-        {
-          label: "Camping Tour",
-          value: { txt: "Camping", class: "fas fa-campground" },
-          id: "t103",
-        },
-        {
-          label: "For Children",
-          value: { txt: "For Children", class: "fas fa-child" },
-          id: "t104",
-        },
-        {
-          label: "Urbanic",
-          value: { txt: "Urbanic Tour", class: "fas fa-city" },
-          id: "t105",
-        },
-        {
-          label: "Nature",
-          value: { txt: "Nature Tour", value: "fas fa-tree" },
-          id: "t106",
-        },
-        {
-          label: "Food",
-          value: { txt: "Food Included", value: "fas fa-utensils" },
-          id: "t107",
-        },
-      ],
-    };
-  },
-  computed: {},
-  created() {
-    const id = this.$route.params.tourId;
-    this.loadTour(id);
-  },
-  methods: {
-    saveImg(url) {
-      this.tourToEdit.imgs.push(url);
+    name: 'tourEdit',
+    data() {
+        return {
+            tourToEdit: null,
+            options: [
+                {
+                    label: 'Water Trail',
+                    value: { txt: 'Water Trail', class: 'fas fa-water' },
+                    id: 't101',
+                },
+                {
+                    label: 'Hiking Tour',
+                    value: { txt: 'Hiking Tour', class: 'fas fa-hiking' },
+                    id: 't102',
+                },
+                {
+                    label: 'Camping Tour',
+                    value: { txt: 'Camping', class: 'fas fa-campground' },
+                    id: 't103',
+                },
+                {
+                    label: 'For Children',
+                    value: { txt: 'For Children', class: 'fas fa-child' },
+                    id: 't104',
+                },
+                {
+                    label: 'Urbanic',
+                    value: { txt: 'Urbanic Tour', class: 'fas fa-city' },
+                    id: 't105',
+                },
+                {
+                    label: 'Nature',
+                    value: { txt: 'Nature Tour', value: 'fas fa-tree' },
+                    id: 't106',
+                },
+                {
+                    label: 'Food',
+                    value: { txt: 'Food Included', value: 'fas fa-utensils' },
+                    id: 't107',
+                },
+            ],
+        };
     },
-    async saveTour() {
-      try {
-        await this.$store.dispatch({
-          type: "saveTour",
-          tour: this.tourToEdit,
-        });
-        this.$router.push(`/`);
-      } catch (err) {
-        console.log("Cannot Save Tour", err);
-      }
+    computed: {},
+    created() {
+        const id = this.$route.params.tourId;
+        this.loadTour(id);
     },
-    async loadTour(id) {
-      try {
-        const tour = await this.$store.dispatch({
-          type: "getTourToEdit",
-          id,
-        });
-        this.tourToEdit = tour;
-        console.log("this.tourToEdit", this.tourToEdit);
-      } catch (err) {
-        console.log("Cannot load Tour", err);
-      }
+    methods: {
+        saveImg(url) {
+            this.tourToEdit.imgs.push(url);
+        },
+        async saveTour() {
+            try {
+                console.log(' this.tourToEdit:', this.tourToEdit);
+                await this.$store.dispatch({
+                    type: 'saveTour',
+                    tour: this.tourToEdit,
+                });
+                this.$router.push(`/`);
+            } catch (err) {
+                console.log('Cannot Save Tour', err);
+            }
+        },
+        async loadTour(id) {
+            try {
+                const tour = await this.$store.dispatch({
+                    type: 'getTourToEdit',
+                    id,
+                });
+                this.tourToEdit = tour;
+                console.log('this.tourToEdit', this.tourToEdit);
+            } catch (err) {
+                console.log('Cannot load Tour', err);
+            }
+        },
+        setCheckPoints(cp) {
+            this.tourToEdit.locs = cp;
+        },
     },
-    setCheckPoints(cp) {
-      this.tourToEdit.locs = cp;
+    components: {
+        tourMap,
+        uploadImage,
     },
-  },
-  components: {
-    tourMap,
-    uploadImage,
-  },
 };
 </script>
